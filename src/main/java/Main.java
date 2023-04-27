f77e592d3106952b9a64cded59915804fef28ba0
Alena Nik <alyona_nik@mail.ru>
2023-04-27 15:13:48 +0400

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         long[] sales = {
-                12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12
+                12, 5, 8, 4, 5, 3, 28, 6, 11, 11, 12
         };
         StatisticsService service = new StatisticsService();
         long findMax = service.findMax(sales);
